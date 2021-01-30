135b0e0394c697d297623249ff8622a9149a85a6
Andy <andy.gehlan@gmail.com>
2021-01-29 23:46:31 -0500

    footer nav color changes based on panel

--- index.js
+++ index.js
@@ -53,3 +53,46 @@ document.querySelector(".submitLeftBack").addEventListener("click" , function(){
   right.style.width = "10%";
   right.style.backgroundColor = "#f8dc81";
 });
+
+// Responsive Navbar (Copied from above)
+
+// If you click to submit on the welcoming page, it moves to suggestions page
+document.querySelector(".submitLeftNext").addEventListener("click" , function(){
+  var step1 = document.querySelector(".step1");
+  step1.style.backgroundColor = "#f8dc81";
+  var step2 = document.querySelector(".step2");
+  step2.style.backgroundColor = "#3cb043";
+  var step3 = document.querySelector(".step3");
+  step3.style.backgroundColor = "#f8dc81";
+
+});
+
+// If you click to back on the suggestion page, it goes back to welcoming page
+document.querySelector(".submitMidBack").addEventListener("click" , function(){
+  var step1 = document.querySelector(".step1");
+  step1.style.backgroundColor = "#3cb043";
+  var step2 = document.querySelector(".step2");
+  step2.style.backgroundColor = "#f8dc81";
+  var step3 = document.querySelector(".step3");
+  step3.style.backgroundColor = "#f8dc81";
+});
+
+// If you click to next on the suggestion page, it moves to ordering page
+document.querySelector(".submitMidNext").addEventListener("click" , function(){
+  var step1 = document.querySelector(".step1");
+  step1.style.backgroundColor = "#f8dc81";
+  var step2 = document.querySelector(".step2");
+  step2.style.backgroundColor = "#f8dc81";
+  var step3 = document.querySelector(".step3");
+  step3.style.backgroundColor = "#3cb043";
+});
+
+// If you click to back on the ordering page, it moves to suggestion page
+document.querySelector(".submitLeftBack").addEventListener("click" , function(){
+  var step1 = document.querySelector(".step1");
+  step1.style.backgroundColor = "#f8dc81";
+  var step2 = document.querySelector(".step2");
+  step2.style.backgroundColor = "#3cb043";
+  var step3 = document.querySelector(".step3");
+  step3.style.backgroundColor = "#f8dc81";
+});
