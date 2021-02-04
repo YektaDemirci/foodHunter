5ecc775bb5ca0c8221d7214dd9064938fd9e3b69
Andy <andy.gehlan@gmail.com>
2021-02-03 21:19:06 -0500

    refactored to reduce styling change logic

--- index.js
+++ index.js
@@ -55,44 +55,35 @@ document.querySelector(".submitLeftBack").addEventListener("click" , function(){
 });
 
 // Responsive Navbar (Copied from above)
+let footerIDSteps = ['step1', 'step2', 'step3'];
+function updateFooter(activeStepID) {
+    footerIDSteps.forEach(step => {
+      if(step == activeStepID){
+        let stepNode = document.getElementById(step);
+        stepNode.className = "col-md current-step";
+      } 
+      else {
+        let stepNode = document.getElementById(step);
+        stepNode.className = "col-md bg-step";
+      }
+      console.log("done");
+    })
+  }
 
 // If you click to submit on the welcoming page, it moves to suggestions page
-document.querySelector(".submitLeftNext").addEventListener("click" , function(){
-  var step1 = document.querySelector(".step1");
-  step1.style.backgroundColor = "#f8dc81";
-  var step2 = document.querySelector(".step2");
-  step2.style.backgroundColor = "#3cb043";
-  var step3 = document.querySelector(".step3");
-  step3.style.backgroundColor = "#f8dc81";
-
+document.querySelector(".submitLeftNext").addEventListener("click", function() {
+  updateFooter("step2");
 });
-
 // If you click to back on the suggestion page, it goes back to welcoming page
-document.querySelector(".submitMidBack").addEventListener("click" , function(){
-  var step1 = document.querySelector(".step1");
-  step1.style.backgroundColor = "#3cb043";
-  var step2 = document.querySelector(".step2");
-  step2.style.backgroundColor = "#f8dc81";
-  var step3 = document.querySelector(".step3");
-  step3.style.backgroundColor = "#f8dc81";
+document.querySelector(".submitMidBack").addEventListener("click", function() {
+  updateFooter("step1");
+});
+// // If you click to next on the suggestion page, it moves to ordering page
+document.querySelector(".submitMidNext").addEventListener("click", function() {
+  updateFooter("step3");
+});
+// // If you click to back on the ordering page, it moves to suggestion page
+document.querySelector(".submitLeftBack").addEventListener("click", function() {
+  updateFooter("step2");
 });
 
-// If you click to next on the suggestion page, it moves to ordering page
-document.querySelector(".submitMidNext").addEventListener("click" , function(){
-  var step1 = document.querySelector(".step1");
-  step1.style.backgroundColor = "#f8dc81";
-  var step2 = document.querySelector(".step2");
-  step2.style.backgroundColor = "#f8dc81";
-  var step3 = document.querySelector(".step3");
-  step3.style.backgroundColor = "#3cb043";
-});
-
-// If you click to back on the ordering page, it moves to suggestion page
-document.querySelector(".submitLeftBack").addEventListener("click" , function(){
-  var step1 = document.querySelector(".step1");
-  step1.style.backgroundColor = "#f8dc81";
-  var step2 = document.querySelector(".step2");
-  step2.style.backgroundColor = "#3cb043";
-  var step3 = document.querySelector(".step3");
-  step3.style.backgroundColor = "#f8dc81";
-});
