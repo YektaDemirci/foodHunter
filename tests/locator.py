ffcd030d861de94bb935c2dd100025b84d60cb9e
Andy <andy.gehlan@gmail.com>
2021-02-04 14:05:27 -0500

    inital test setup w/ static page test

---
+++ tests/locator.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators(object):
+    FOOTER_STEP1 = (By.ID, "step1")
+    FOOTER_STEP2 = (By.ID, "step2")
+    FOOTER_STEP3 = (By.ID, "step3")
+
+    
---
+++ tests/page.py
@@ -0,0 +1,27 @@
+from locator import MainPageLocators
+from element import BasePageElement
+
+class SearchFooterElement(BasePageElement):
+    locator = "step1"
+
+class BasePage(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+# define 1 class for 1 page or 3 classes for 3 panels
+
+class MainPage(BasePage):
+
+    search_footer_element = SearchFooterElement()
+
+    def is_footer_step1_highlighted(self):
+        elementActive = self.driver.find_element(*MainPageLocators.FOOTER_STEP1)
+        classNameActive = elementActive.get_attribute("class")
+
+        elementPassive1 = self.driver.find_element(*MainPageLocators.FOOTER_STEP2)
+        classNamePassive1 = elementPassive1.get_attribute("class")
+
+        elementPassive2 = self.driver.find_element(*MainPageLocators.FOOTER_STEP3)
+        classNamePassive2 = elementPassive2.get_attribute("class")
+
+        return (("current-step" in classNameActive) and ("bg-step" in classNamePassive1 and "bg-step" in classNamePassive2))
