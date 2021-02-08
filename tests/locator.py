0cec389d04f923e4f4c049a6529ab6c132ed3e27
Andy <andy.gehlan@gmail.com>
2021-02-08 15:26:11 -0500

    Updated footer and footer tests to work with updated variable names

--- tests/locator.py
+++ tests/locator.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
-    FOOTER_STEP1 = (By.ID, "step1")
-    FOOTER_STEP2 = (By.ID, "step2")
-    FOOTER_STEP3 = (By.ID, "step3")
+    # footer prefixes because step# by className returned panel divs instead of footer
+    FOOTER_STEP1 = (By.ID, "footer-step1")
+    FOOTER_STEP2 = (By.ID, "footer-step2")
+    FOOTER_STEP3 = (By.ID, "footer-step3")
 
     
--- tests/page.py
+++ tests/page.py
@@ -8,7 +8,7 @@ class BasePage(object):
     def __init__(self, driver):
         self.driver = driver
 
-# define 1 class for 1 page or 3 classes for 3 panels
+# define 1 class for 1 page or 3 classes for 3 panels?
 
 class MainPage(BasePage):
 
@@ -24,4 +24,4 @@ class MainPage(BasePage):
         elementPassive2 = self.driver.find_element(*MainPageLocators.FOOTER_STEP3)
         classNamePassive2 = elementPassive2.get_attribute("class")
 
-        return (("current-step" in classNameActive) and ("bg-step" in classNamePassive1 and "bg-step" in classNamePassive2))
+        return (("current-step" in classNameActive) and ("undone-step" in classNamePassive1 and "undone-step" in classNamePassive2))
