cbc0d61cd72b5d2058264409025c099564f9e5ff
Yekta <>
2021-02-10 15:16:57 -0500

    added test cases for the sliding bar

--- tests/page.py
+++ tests/page.py
@@ -23,14 +23,11 @@ class MainPage(BasePage):
         className = element.get_attribute("class")
         return style in className
 
-    def isFooterStylingCorrect(self, array):
+    def isStylingCorrect(self, array):
         correctStyle = []
         for testCase in array:
             correctStyle.append( self.isStepHighlighted(testCase[0], testCase[1]) )
         return True if False not in correctStyle else False
-
-    def is_footer_step1_highlighted(self):
-        return ( self.isFooterStylingCorrect(MainPageLocators.STEP_1_HIGHLIGHTED) )
 
     def inputIngredientAndClickSubmit(self):
         searchElement = self.driver.find_element(*MainPageLocators.SEARCH_BOX)
@@ -38,16 +35,50 @@ class MainPage(BasePage):
 
         submitElement = self.driver.find_element(*MainPageLocators.SUBMIT_BUTTON)
         submitElement.click()
+
+    def is_footer_step1_highlighted(self):
+        return ( self.isStylingCorrect(MainPageLocators.STEP_1_HIGHLIGHTED) )
     
     def is_footer_step2_highlighted(self):
         self.inputIngredientAndClickSubmit()
-        return ( self.isFooterStylingCorrect(MainPageLocators.STEP_2_HIGHLIGHTED) )
+        return ( self.isStylingCorrect(MainPageLocators.STEP_2_HIGHLIGHTED) )
 
     def is_footer_step3_highlighted(self):
         self.inputIngredientAndClickSubmit()
         nextButtonElement = self.driver.find_element(*MainPageLocators.MID_NEXT_BUTTON)
         nextButtonElement.click()
-        return ( self.isFooterStylingCorrect(MainPageLocators.STEP_3_HIGHLIGHTED) )
+        return ( self.isStylingCorrect(MainPageLocators.STEP_3_HIGHLIGHTED) )
+    
+    #The test to check if the boxes have right styles in step 1
+    def is_box_step1_highlighted(self):
+        return ( self.isStylingCorrect(MainPageLocators.STEP_1_BOX) )
+    
+    #Test to check if the boxes have right styles: step1->step2
+    def is_box_step2_highlighted(self):
+        self.inputIngredientAndClickSubmit()
+        return ( self.isStylingCorrect(MainPageLocators.STEP_2_BOX) )
+
+    #Test to check if the boxes have right styles: step1->step2->step3
+    def is_box_step3_highlighted(self):
+        self.inputIngredientAndClickSubmit()
+        self.driver.find_element(*MainPageLocators.MID_NEXT_BUTTON).click()
+        return ( self.isStylingCorrect(MainPageLocators.STEP_3_BOX) )
+
+    #Test to check if the boxes have right styles: step1->step2->step3->step2
+    def is_box_step2_rehighlighted(self):
+        self.inputIngredientAndClickSubmit()
+        self.driver.find_element(*MainPageLocators.MID_NEXT_BUTTON).click()
+        self.driver.find_element(*MainPageLocators.RIGHT_BACK_BUTTON).click()
+        return ( self.isStylingCorrect(MainPageLocators.STEP_2_BOX) )
+
+    #Test to check if the boxes have right styles: step1->step2->step3->step2->step1
+    def is_box_step1_rehighlighted(self):
+        self.inputIngredientAndClickSubmit()
+        self.driver.find_element(*MainPageLocators.MID_NEXT_BUTTON).click()
+        self.driver.find_element(*MainPageLocators.RIGHT_BACK_BUTTON).click()
+        time.sleep(0.5)
+        self.driver.find_element(*MainPageLocators.MID_BACK_BUTTON).click()
+        return ( self.isStylingCorrect(MainPageLocators.STEP_1_BOX) )
 
 
     
