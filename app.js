00d00d13958da615301b759e30e4c0436d6a8195
@ydemirci <ydemirci@uwaterloo.ca>
2021-03-21 14:09:49 -0400

    a new database created, tags and ingredients are merged

--- app.js
+++ app.js
@@ -2,7 +2,19 @@
 const express = require("express")
 const bodyParser = require("body-parser");
 const _ = require("lodash");
+const mongoose = require('mongoose');
 
+mongoose.connect("mongodb+srv://foodHunter:1hunt1@cluster0.t1di3.mongodb.net/fhDB",{useNewUrlParser: true});
+
+const restaurantSchema = {
+  restaurant: String,
+	product: String,
+	tags: [String],
+	address: String
+};
+
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,12 +26,21 @@ app.use(express.static("public"))
 app.get("/", function (req, res) {
   res.sendFile(__dirname+"/main.html");})
 
+// to get the input from the client and search the database
 app.post("/", function(req,res){
-    // The input is turned into lowerCase then parsed, based on comma, space, semicolumn, dot
-    // The tests are "chicken,beef PArsley" & "PARSLEY chicKEn"
+    // The input is turned into lowerCase then parsed, based on comma, semicolumn, dot {\s for space}
+    // The tests are "chicken,beef,PArsley" & "PARSLEY.chicKEn"
     // I can populate the tests
-    var input = _.words([req.body.search.toLowerCase()], /[^,.\s;]+/g);
-    // console.log(input);
+    var input = _.words([req.body.search.toLowerCase()], /[^,.;]+/g);
+    console.log(input);
+     // Restaurant.find({ ingredients: input}, function (err, foundList) {
+     //   if(!err){
+     //     console.log(foundList);
+     //   } else {
+     //     console.log(err);
+     //   }
+     // });
+
 
   });
 
--- updateDB2.js
+++ updateDB2.js
@@ -1,40 +1,47 @@
 // We need to install mongoose through <npm i mongoose>, a tool to access mongoDB via node
 const mongoose = require('mongoose');
 const fs = require('fs');
+const _ = require("lodash");
+
 
 // id:foodHunter
 // pw:1hunt1
-mongoose.connect("mongodb+srv://foodHunter:1hunt1@cluster0.t1di3.mongodb.net/foodHunterDB",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://foodHunter:1hunt1@cluster0.t1di3.mongodb.net/fhDB",{useNewUrlParser: true});
 
-let rawdata = fs.readFileSync('public/data_food_sample.json');
-let restaurants = JSON.parse(rawdata);
+let jsonFood = fs.readFileSync('public/data_food_sample.json');
+let restaurants = JSON.parse(jsonFood);
+
+
 
 // The schema according to our json file
 const restaurantSchema = {
   restaurant: String,
 	product: String,
-	ingredients: String,
-	tags: String,
+	tags: [String],
 	address: String
 };
 
 const Restaurant = mongoose.model("Restaurant", restaurantSchema);
-const rests = []
+
+
+const rests = [];
 
 for (const rest of restaurants){
+
+  var parsedIngredients = _.words([rest.ingredients.toLowerCase()], /[^,.;]+/g);
+  var parsedTags = _.words([rest.tags.toLowerCase()], /[^,.;]+/g);
+
+  parsedTags=parsedTags.concat(parsedIngredients);
+
   const rest1 = new Restaurant({
     restaurant: rest.restaurant,
   	product: rest.product,
-  	ingredients: rest.ingredients,
-  	tags: rest.tags,
+  	tags: parsedTags,
   	address: rest.address
   });
   rests.push(rest1);
 }
 
-console.log("End of for loop");
-
-//The following will save all to the DB
 Restaurant.insertMany(rests, function(err){
   if(err){
     console.log(err);
@@ -42,3 +49,10 @@ Restaurant.insertMany(rests, function(err){
     console.log("Successfully saved the default items to DBs, please close the execution with CTRL+C");
   }
 });
+
+// db.restaurants.find(
+//     {$or:[
+//         {ingredients:{ $in :["chicken"]}},
+//         {tags:{ $in:["nonveg"]}}
+//     ]}
+// )
