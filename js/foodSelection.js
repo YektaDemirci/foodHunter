ec1956e6af51e6553f82197398764bfbc6d163de
Rahul Punchhi <rpunchhi@uwaterloo.ca>
2021-04-01 10:37:04 -0400

    UI updated, quick fix to avoid duplicate order selections

--- js/foodSelection.js
+++ js/foodSelection.js
@@ -4,12 +4,13 @@ function food_selection(value) {
     var value_split = value.split("??");
     var selection_str = "<div class=\"selection-div-class\">" + value_split[0] + " from " + value_split[1] + " located at " + value_split[2] + "<br>";
     value = value.replace("'", "");
-    selection_str += "<button class=\"submitLeftNext btn btn-light\" id = \"clear\" type=\"submit\" onclick=\"order_now('" + value + "')\">Place Order <i class=\"fas fa-shopping-cart\"></i></button>";
+    selection_str += "<button class=\"submitLeftNext btn btn-light\" type=\"submit\" onclick=\"order_now('" + value + "')\">Place Order <i class=\"fas fa-shopping-cart\"></i></button>";
     selection_str += "</div>";
     console.log(selection_str);
     var selections = document.getElementById('selections').innerHTML;
+    var check_if_selected = selections.includes(value_split[0]) && selections.includes(value_split[1]) && selections.includes(value_split[2]);
 
-    if(!selections.includes(selection_str)){
+    if(!check_if_selected){
         selections += selection_str;
         document.getElementById('selections').innerHTML = selections;
     }
