9c2f0f0b4a1fd60f1e74599beb8f6bfcf0f43d26
YektaDemirci <ydemirci@uwaterloo.ca>
2021-04-02 16:40:09 -0400

    mongoDB server is updated acc to documenu, Seattle-UoW

---
+++ mongoDBupdate.py
@@ -0,0 +1,58 @@
+import requests
+import json
+import math
+import pymongo
+
+key = "a7e037de3731e07c5ad9ebbaedcf25da";
+#The centre is University of Washington acc to lat and lon
+lat = "47.655548";
+lon = "-122.303200";
+#Radius acc to the centre
+dist = "1";
+url = "https://api.documenu.com/v2/restaurants/search/geo?lat="+lat+"&lon="+lon+"&distance="+dist+"&fullmenu&key="+key;
+
+r =requests.get(url)
+
+d_dict = r.json()
+
+#print(d_dict["data"][0]["restaurant_name"])
+totalRest = d_dict["totalResults"]
+totalPage = math.ceil(totalRest/25)
+totalPage = totalPage+1;
+
+mongo_list = []
+
+for page in range(1,totalPage):
+    url = "https://api.documenu.com/v2/restaurants/search/geo?lat="+lat+"&lon="+lon+"&distance="+dist+"&page="+str(page)+"&fullmenu&key="+key;
+    r =requests.get(url)
+    d_dict = r.json()
+    for i in d_dict["data"]:
+        for j in i["menus"]:
+            if(j["menu_name"] == "Main"):
+                for k in j["menu_sections"]:
+                    for l in k["menu_items"]:
+                        dish={
+                        "restaurant": i["restaurant_name"],
+                        "product": l["name"],
+                        "ingredients": l["description"],
+                        "address": i["address"]["formatted"],
+                        "price": l["price"]
+                        }
+                        mongo_list.append(dish)
+    print("Page "+str(page)+" is completed, total page number is: "+str(totalPage))
+
+myclient = pymongo.MongoClient("mongodb+srv://foodHunter:1hunt1@cluster0.t1di3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mydb = myclient["foodHunterDB"]
+mycol = mydb["restaurantsExtended"]
+
+#Instead of checking and updateding the changed things, I delete and rewrite
+print("Deleting the old tables")
+mycol.drop()
+mycol.insert_many(mongo_list)
+print("MongoDB (foodHunterDB/restaurantsExtended) is updated")
+
+#print( len(mongo_list) )
+#print(d_dict["data"][0]["menus"])
+
+# for i in d_dict["data"]:
+#     print(i["address"]["formatted"])
